a8145a0ea42cb25fad2cf4bb061a267e25952d9f
Julyan Bizeranov <julyan997@gmail.com>
2024-07-05 00:30:02 +0300

    added reconnection logic

--- ControllerRebinder.Core/XboxControllerBinder.cs
+++ ControllerRebinder.Core/XboxControllerBinder.cs
@@ -2,7 +2,7 @@
 using ControllerRebinder.Core.Caches;
 using ControllerRebinder.Core.Events;
 using ControllerRebinder.Core.Services;
-using ControllerRebinder.Core.Services.Imp;// Add this line to include the handler namespace
+using ControllerRebinder.Core.Services.Imp; // Add this line to include the handler namespace
 using DXNET.XInput;
 using Microsoft.Extensions.Logging;
 using System;
@@ -19,8 +19,8 @@ namespace ControllerRebinder.Core
         private readonly IJoyStickService _leftJoyStickService;
         private readonly IJoyStickService _rightJoyStickService;
         private readonly IButtonsService _buttonsService;
-        private readonly JoyStickHandler _leftJoyStickHandler;  
-        private readonly JoyStickHandler _rightJoyStickHandler; 
+        private readonly JoyStickHandler _leftJoyStickHandler;
+        private readonly JoyStickHandler _rightJoyStickHandler;
 
         public XboxControllerBinder(Controller controller, InputSimulator inputSimulator, ILogger<XboxControllerBinder> logger)
         {
@@ -52,17 +52,33 @@ namespace ControllerRebinder.Core
 
         public async Task Start()
         {
-            if (ConfigCache.Configurations.LeftJoyStick.On)
+            while (true)
             {
-                _ = _leftJoyStickService.Start();
-            }
-            if (ConfigCache.Configurations.RightJoyStick.On)
-            {
-                _ = _rightJoyStickService.Start();
-            }
-            if (ConfigCache.Configurations.Buttons.On)
-            {
-                _ = _buttonsService.Start();
+                if (_controller.IsConnected)
+                {
+                    _logger.LogInformation("Controller connected.");
+
+                    if (ConfigCache.Configurations.LeftJoyStick.On)
+                    {
+                        _ = _leftJoyStickService.Start();
+                    }
+                    if (ConfigCache.Configurations.RightJoyStick.On)
+                    {
+                        _ = _rightJoyStickService.Start();
+                    }
+                    if (ConfigCache.Configurations.Buttons.On)
+                    {
+                        _ = _buttonsService.Start();
+                    }
+
+                    break; // Exit the loop once the controller is connected and services are started
+                }
+                else
+                {
+                    _logger.LogWarning("Controller not connected. Attempting to reconnect...");
+                }
+
+                await Task.Delay(1000); // Wait for 1 second before retrying
             }
 
             await Task.Run(() => Console.ReadLine());
